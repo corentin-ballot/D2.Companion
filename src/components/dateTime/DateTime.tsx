2edc7a708d8b7ebe1b8a0e08e3c760e7121fa101
Corentin BALLOT <corentin.ballot@gmail.com>
2022-03-31 22:36:40 +0200

    remove seconds from DateTime component

--- src/components/dateTime/DateTime.tsx
+++ src/components/dateTime/DateTime.tsx
@@ -6,8 +6,8 @@ interface DateTimeProps {
 
 function DateTime(props: DateTimeProps) {
     const date = new Date(props.timestamp);
-    const formatedDateTime = new Intl.DateTimeFormat('fr-FR', { hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(date);
-    return <span data-timestamp={props.timestamp}>{formatedDateTime}</span>
+    const formatedDateTime = new Intl.DateTimeFormat('fr-FR', { hour: '2-digit', minute: '2-digit'}).format(date);
+    return <span data-timestamp={props.timestamp}>[{formatedDateTime}]</span>
 }
 
 export default DateTime;
