31ca0785f0edee8dc5ef20d71cc40c2ae4bc58bf
Corentin BALLOT <corentin.ballot@gmail.com>
2022-07-22 17:16:23 +0200

    update fetch methods

---
+++ src/data/fetch_all.js
@@ -0,0 +1,7 @@
+const fetchMonsters = require("./fetch_monsters");
+const fetchEquipments = require("./fetch_equipments");
+const fetchClasses = require("./fetch_classes");
+
+// fetchMonsters();
+fetchEquipments();
+// fetchClasses();
