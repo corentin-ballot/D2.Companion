5a97cd0dc67f2fceaddaeaf0abb4bf4553f3ad9c
Corentin BALLOT <corentin.ballot@gmail.com>
2022-07-22 17:15:07 +0200

    remove sample reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import socketMiddleWare from './socketMiddleWare'
-import counterReducer from '../features/counter/counterSlice';
 import chatReducer from '../features/chat/chatSlice';
 import socketReducer from '../features/socket/socketSlice';
 import fightsReducer from '../features/fights/fightsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     chat: chatReducer,
     socket: socketReducer,
     fights: fightsReducer,
