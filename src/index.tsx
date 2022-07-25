8f3586876ea84ccc18367e08ffe222feb11073c6
Corentin BALLOT <corentin.ballot@gmail.com>
2022-07-25 22:36:23 +0200

    fix router base url

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
