0f320192a13a46ba3bed3044b0efcc575e87d8a1
Corentin BALLOT <corentin.ballot@gmail.com>
2023-06-22 13:49:38 +0200

    update mounts fetch

--- src/data/fetch_all.js
+++ src/data/fetch_all.js
@@ -2,10 +2,10 @@ const fetchMonsters = require("./fetch_monsters");
 const fetchEquipments = require("./fetch_equipments");
 const fetchClasses = require("./fetch_classes");
 const fetchAchievmentCategories = require("./fetch_achievments");
-const fetchMounts = require("./fetch_mounts");
 const fetchQuestsCategories = require("./fetch_quests");
 const fetchAlmanax = require("./fetch_almanax");
 const fetchItems = require("./fetch_items");
+const fetchMounts = require("./fetch_mounts_v2");
 
 // fetchMonsters();
 // fetchEquipments();
@@ -14,4 +14,5 @@ const fetchItems = require("./fetch_items");
 // fetchMounts();
 // fetchQuestsCategories();
 // fetchAlmanax();
-fetchItems();
+// fetchItems();
+fetchMounts();
